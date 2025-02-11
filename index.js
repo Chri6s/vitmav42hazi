c91e7b4cf062beee066fac4a7a593a3bd8faeefd
FrimeX <pistahozak@gmail.com>
2025-02-11 18:18:47 +0100

    Add shit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-let express = require("express");
+const express = require("express");
 let app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+const Cat = mongoose.model('Cat', { name: String });
 
 let port = 3000;
+
 app.get("/", (req, res, next) => {
-    res.send('Hello vro<3');
+    const kitty = new Cat({ name: 'Zildjian' });
+    kitty.save().then(() => console.log('meow'));
+    res.send('<h1>Hello vro<3 I did something in the background!</h1>');
+
 });
 
 let server = app.listen(port, () => {
