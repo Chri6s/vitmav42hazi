6a26086cd2e2c224e868edaba833e19911ae275d
FrimeX <pistahozak@gmail.com>
2025-02-12 15:45:51 +0100

    Added libs and specification

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
-let app = express();
 const mongoose = require('mongoose');
+const { ConfigHandler } = require("./lib/js/ConfigHandler");
+let app = express();
+let configHandler = new ConfigHandler();
+let port = configHandler.get("serverOptions", "port");
 
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 const Cat = mongoose.model('Cat', { name: String });
 
-let port = 3000;
+
 
 app.get("/", (req, res, next) => {
     const kitty = new Cat({ name: 'Zildjian' });
     kitty.save().then(() => console.log('meow'));
-    res.send('<h1>Hello vro<3 I did something in the background!</h1>');
+    res.send(`I'm running on port: ${configHandler.get("serverOptions", "port")}`);
 
 });
 
