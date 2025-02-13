eb97a0e74058c313b290559e0f2f0f8da3d2994e
Chri6s <krisz@noreply.git.gundalfwastaken.com>
2025-02-13 23:23:50 +0100

    Added db.js

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require("express");
-const mongoose = require('mongoose');
 const { ConfigHandler } = require("./lib/js/ConfigHandler");
-let app = express();
 let configHandler = new ConfigHandler();
 let port = configHandler.get("serverOptions", "port");
+let app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
-const Cat = mongoose.model('Cat', { name: String });
-
-
+app.set("view engine", "ejs");
 
 app.get("/", (req, res, next) => {
-    const kitty = new Cat({ name: 'Zildjian' });
-    kitty.save().then(() => console.log('meow'));
     res.send(`I'm running on port: ${configHandler.get("serverOptions", "port")}`);
-
 });
 
 let server = app.listen(port, () => {
