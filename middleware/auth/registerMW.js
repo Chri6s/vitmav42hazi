6fb2c6d79531a310ac69689ad58612e69bc41d0a
Chri6s <business.frimex@gmail.com>
2025-03-20 23:30:47 +0100

    I did something and now its good

--- middleware/auth/registerMW.js
+++ middleware/auth/registerMW.js
@@ -1,71 +1,72 @@
 const requireOption = require("../../lib/js/requireOption");
 const crypto = require('crypto');
+const colors = require("yoctocolors");
 
 module.exports = function(objectRepository) {
     const UserModel = requireOption(objectRepository, 'UserModel');
 
-    return function(req, res, next) {
+    return async function(req, res, next) {
+        if (req.method !== 'POST') {
+            return next();
+        }
         const regInfo = req.body;
-        if (req.method !== 'POST') return next();
-        if (!(regInfo.username || regInfo.email || regInfo.password)) {
+
+        if (!regInfo.username || !regInfo.email || !regInfo.password) {
             res.locals.error = "All fields are required";
             return next();
         }
-        UserModel.findOne({
-            $or: [
-                { username: regInfo.username },
-                { email: regInfo.email }
-            ]
-        })
-        .then(existingUser => {
-            if(existingUser) {
-                if(existingUser.username === regInfo.username) {
+
+        try {
+            const existingUser = await UserModel.findOne({
+                $or: [
+                    { username: regInfo.username },
+                    { email: regInfo.email }
+                ]
+            });
+
+            if (existingUser) {
+                if (existingUser.username === regInfo.username) {
                     res.locals.error = "Username already taken";
                 } else {
                     res.locals.error = "Email already taken";
                 }
                 return next();
-            } 
+            }
             const hashedPassword = crypto.createHash('sha256')
                 .update(regInfo.password)
                 .digest('hex');
             
-            UserModel.findOne().sort('-id').exec()
-                .then(lastUser => {
-                    const newId = lastUser ? lastUser.id + 1 : 1;
+            const lastUser = await UserModel.findOne().sort('-id').exec();
+            const newId = lastUser ? lastUser.id + 1 : 1;
 
-                    const newUser = new UserModel({
-                        id: newId,
-                        username: regInfo.username,
-                        email: regInfo.email,
-                        gravatarEmail: null,
-                        password: hashedPassword,
-                        type: 'user'
-                    });
+            const newUser = new UserModel({
+                id: newId,
+                username: regInfo.username,
+                email: regInfo.email,
+                gravatarEmail: null,
+                password: hashedPassword,
+                type: 'user'
+            });
 
-                    return newUser.save();
-                })
-                .then(savedUser => {
-                    req.session.userid = savedUser._id;
+            const savedUser = await newUser.save();
+            
+            req.session.userid = savedUser._id;
 
-                    if(req.headers['content-type'] === 'application/json') {
-                        return res.status(201).json({
-                            success: true,
-                            message: "Registration successful"
-                        });
-                    }
-                    return res.redirect('/user');
-                })
-                .catch(err => {
-                    console.log(`[${colors.cyan("MW")}][❌] Error saving user:\n`, err);
-                    res.locals.error = "An error occurred during registration";
-                    return next();
+            req.session.successMessage = "Registration successful! Welcome to BookJam!";
+            
+            if (req.headers['content-type'] === 'application/json') {
+                return res.status(201).json({
+                    success: true,
+                    message: "Registration successful"
                 });
-        })
-        .catch(err => {
-            console.log(`[${colors.cyan("MW")}][❌] Error checking existing user:\n`, err);
+            }
+            
+            return res.redirect('/user');
+            
+        } catch (err) {
+            console.log(`[${colors.cyan("MW")}][❌] Registration error:`, err);
             res.locals.error = "An error occurred during registration";
             return next();
-        });
-    }
-}
+        }
+    };
+};
--- middleware/renderMW.js
+++ middleware/renderMW.js
@@ -3,6 +3,12 @@ const requireOption = require('../lib/js/requireOption');
 
 module.exports = function(objectrepository, viewName) {
     return function(req, res) {
-        res.render(viewName);
+        res.render(viewName, {
+            successMessage: req.session.successMessage || null
+        });
+        
+        if (req.session.successMessage) {
+            req.session.successMessage = null;
+        }
     };
 };
